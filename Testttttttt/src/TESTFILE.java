037484aab856b6190d7c0b56b81eef7a4ea3d42c
Jason <shizong@outlook.com>
2019-08-27 14:58:40 +1200

    This is the test code to test directory of files after commit

---
+++ Testttttttt/src/TESTFILE.java
@@ -0,0 +1,16 @@
+/**
+ * @program Testttttttt
+ * @description: TEST
+ * @author: Zong Shi
+ * @create 2019-08-27 14:53
+ */
+
+public class TESTFILE {
+  public static void main(String[] args) {
+
+    String a = "Test";
+    System.out.println("This is the test file");
+
+  }
+}
+    
